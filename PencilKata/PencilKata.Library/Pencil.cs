358537dde8581284109bd705833f4cba11a1c7c1
John MacGregor <jonathan.macgregor@accenture.com>
2019-04-19 15:55:46 -0400

    Pencil writes spaces when durability runs out

--- PencilKata/PencilKata.Library/Pencil.cs
+++ PencilKata/PencilKata.Library/Pencil.cs
@@ -2,9 +2,31 @@ namespace PencilKata.Library
 {
     public class Pencil
     {
+        private int _durability;
+
+        public Pencil(int durability)
+        {
+            _durability = durability;
+        }
+        
         public void Write(IWritable paper, string stringToWrite)
         {
-            paper.Write(stringToWrite);
+            var writtenString = string.Empty;
+
+            foreach (var character in stringToWrite)
+            {
+                if (_durability > 0)
+                {
+                    writtenString += character;
+                    _durability--;
+                }
+                else
+                {
+                    writtenString += " ";
+                }
+            }
+            
+            paper.Write(writtenString);
         }
     }
 }
--- PencilKata/PencilKata.Tests/PencilShould.cs
+++ PencilKata/PencilKata.Tests/PencilShould.cs
@@ -10,13 +10,24 @@ namespace PencilKata.Tests
         [Fact]
         public void WriteToPaper()
         {
-            var pencil = new Pencil();
+            var pencil = new Pencil(25);
             var paper = Substitute.For<IWritable>();
             var testString = "We writing nao";
 
             pencil.Write(paper, testString);
 
             paper.Received(1).Write(testString);
+        }
+
+        [Fact]
+        public void ReturnSpacesWhenWritingBeyondDurability()
+        {
+            var pencil = new Pencil(4);
+            var paper = Substitute.For<IWritable>();
+            
+            pencil.Write(paper,"running");
+            
+            paper.Received(1).Write("runn   ");
         }
     }
 }
