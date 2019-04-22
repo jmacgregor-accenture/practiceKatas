81bfa295101671bbbf0db2351f2fba811ad544cc
John MacGregor <jonathan.macgregor@accenture.com>
2019-04-22 13:41:24 -0400

    Point degrades as it writes

--- PencilKata/PencilKata.Library/Pencil.cs
+++ PencilKata/PencilKata.Library/Pencil.cs
@@ -1,10 +1,34 @@
+using System.Runtime.Serialization;
+
 namespace PencilKata.Library
 {
     public class Pencil
     {
+        private int _durability = 4;
+
+        public Pencil(int durability)
+        {
+            _durability = durability;
+        }
+
         public void Write(IWritable surface, string input)
         {
-            surface.Write(input);
+            var stringToWrite = string.Empty;
+
+            foreach (var character in input)
+            {
+                if (_durability > 0)
+                {
+                    stringToWrite += character;
+                    _durability--;
+                }
+                else
+                {
+                    stringToWrite += ' ';
+                }
+            }
+            
+            surface.Write(stringToWrite);
         }
     }
 }
--- PencilKata/PencilKata.Tests/PencilShould.cs
+++ PencilKata/PencilKata.Tests/PencilShould.cs
@@ -12,12 +12,25 @@ namespace PencilKata.Tests
         public void WriteToPaper()
         {
             var paper = Substitute.For<IWritable>();
-            var pencil = new Pencil();
+            var pencil = new Pencil(15);
             var testString = "writing";
 
             pencil.Write(paper, testString);
 
             paper.Received(1).Write(testString);
+        }
+
+        [Fact]
+        public void DegradeAsWrites()
+        {
+            var paper = Substitute.For<IWritable>();
+            var pencil = new Pencil(4);
+            var testString = "writing";
+            var expectedString = "writ   ";
+            
+            pencil.Write(paper, testString);
+            
+            paper.Received(1).Write(expectedString);
         }
     }
 }
